e4aee8aa82bc55c96c4e205bf0c524dc832117c6
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-13 14:10:10 +0530

    skdnaslk

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
 
-openFile = tk.Button(root, text="Open File",padx=10, pady=5, fg="white", bg="#263D42")
+openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#263D42")
+openFile.pack()
 
+runApps = tk.Button(root, text="RUN", padx=10, pady=5, fg="white", bg="#263D42")
+runApps.pack()
 
 root.mainloop()
