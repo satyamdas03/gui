fea288823e43b344d2782681a7bb0896be1b9115
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2023-07-13 14:21:05 +0530

    dfsj:

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from tkinter import filedialog, Text
 import os
 
 root = tk.Tk()
+apps=[]
 
 def addApp():
     filename = filedialog.askopenfilename(initialdir="/", title="Select File",
                                           filetypes=(("executables", "*.exe"), ("all files", "*.*")))
+    apps.append(filename)
+    print(filename)
 
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 canvas.pack()
